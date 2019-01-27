a30c7d272e037ff02f6330fb0f0c3ae8e31426cb
TeriFash <teri.fash@gmail.com>
2019-01-27 21:20:16 +0200

    List app
    Co-Authored-By: TeriFash <teri.fash@gmail.com>

---
+++ src/bd/store.js
@@ -0,0 +1,19 @@
+const currenciesList = [
+  {
+    iso: 'usd',
+    value: '27',
+    name: 'Долар'
+ },
+ {
+   iso: 'rub',
+   value: '463',
+   name: 'Рубль'
+},
+{
+  iso: 'uah',
+  value: '233',
+  name: 'Гривна'
+},
+];
+
+export {currenciesList};
